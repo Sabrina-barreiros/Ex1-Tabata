eba30084b6b981d87d07f7bc00e0ed9f133fe19e
Sabrina-barreiros <83960592+Sabrina-barreiros@users.noreply.github.com>
2023-02-15 20:19:45 -0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -24,12 +24,29 @@ let title = "Temporizador Tabata";
 document.getElementById('app').innerHTML = title;
 
 let action = '-';
-let second = 3;
 let round =0;
+let second = 3;
 let intervalId =  setInterval(() => {
   if (round <1) action = 'Prepare-se!';
   else if (round < 8) action = 'Feito!';
   else if (second > 10 ) action = 'Vamos lá!';
   else action = 'Descanso! Você merece (=';
   document.getElementById('app-acao').innerHTML = action;
-})
+  
+  if (round >=1 && round <=8){
+    document.getElementById('app-round').innerHTML = round.toString();
+  }else {
+    document.getElementById('app-round').innerHTML = '-';
+  }
+  if (round <= 8) {
+    document.getElementById('app-tempo').innerHTML = second.toString() + 's';
+  }else {
+    document.getElementById('app-tempo').innerHTML = '-';
+  }
+
+  second -= 1;
+  if(second == 0){
+    round += 1;
+    second = 30;
+  }
+},1000);
